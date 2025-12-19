6d9732a2c538198abf1f6c94516e67b823122633
pimcorejp <takeshi.hioki.sig+cms@gmail.com>
2025-12-19 10:48:48 +0900

    Field Active

--- src/Resources/public/js/translation-button.js
+++ src/Resources/public/js/translation-button.js
@@ -353,17 +353,6 @@ translateTab: function (targetTab, tabPanel, sourceLang, provider) {
         }
 
         // ソースタブからフィールドを収集
-
-        
-        // ソースタブをアクティブにしてDOMを完全にロード
-        const sourceTabPanel = sourceTab.ownerCt;
-        if (sourceTabPanel && sourceTabPanel.setActiveTab) {
-            sourceTabPanel.setActiveTab(sourceTab);
-        }
-        
-        // DOM更新を待つ
-        await new Promise(resolve => setTimeout(resolve, 200));
-        
         console.log("=== Collecting source fields ===");
         const sourceTranslatableFields = [];
         const sourceCopyableFields = [];
